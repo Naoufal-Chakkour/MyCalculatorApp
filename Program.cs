99f205a93163350d3a9d5bf0700744f59a9b809c
Naoufal-Chakkour <laroussichakkour@gmail.com>
2026-09-05 17:54:39 +0200

    "Clean program.cs"

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.UseStaticFiles(); 
+ 
 app.UseDefaultFiles();
-app.UseStaticFiles(); 
+app.UseStaticFiles();
 
 
 app.MapPost("/calculate", async (HttpContext context) =>
